287ed9959d4e944a3df92e7ed239f4395c709289
bismak02 <bkhan2@hawk.iit.edu>
2023-11-07 21:49:12 -0600

    editing js

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,31 @@
-document.getElementById("emailForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+<script>
+    // Get the current page URL
+    const currentPageURL = window.location.href;
 
-    const emailInput = document.getElementById("email");
-    const confirmationText = document.getElementById("confirmation");
-
-    if (isValidEmail(emailInput.value)) {
-        confirmationText.textContent = "Thank you! Your email has been submitted.";
-        emailInput.value = "";
-    } else {
-        confirmationText.textContent = "Please enter a valid email address.";
+    // Check if the current page URL matches the URL of the "Membership" page
+    if (currentPageURL.includes('index.html')) {
+        // If it's the membership page, make the navigation header darker
+        const navigationHeader = document.getElementById('navigation-header');
+        navigationHeader.classList.add('darker-navigation-header');
     }
-});
 
-function isValidEmail(email) {
-    // Basic email validation using a regular expression
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
+    document.getElementById("emailForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const emailInput = document.getElementById("email");
+        const confirmationText = document.getElementById("confirmation");
+
+        if (isValidEmail(emailInput.value)) {
+            confirmationText.textContent = "Thank you! Your email has been submitted.";
+            emailInput.value = "";
+        } else {
+            confirmationText.textContent = "Please enter a valid email address.";
+        }
+    });
+
+    function isValidEmail(email) {
+        // Basic email validation using a regular expression
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+</script>

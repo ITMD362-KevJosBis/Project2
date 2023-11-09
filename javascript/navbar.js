c25b3129a3e9da29a518740b41b9e0124a7b37a6
Joshua Davenport <87725934+jdavenport2021@users.noreply.github.com>
2023-11-08 23:26:03 -0600

    adding js checker

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,8 +1,12 @@
 function myFunction() {
-    var x = document.getElementById("nav-navigation");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+  var x = document.getElementById("nav-navigation");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});

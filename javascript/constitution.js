8cbc5ffafd1c45c4f51838b5ec111b720cf229d4
Kevin-Alexis-Aguilar <89360177+Kevin-Alexis-Aguilar@users.noreply.github.com>
2023-11-26 17:47:41 -0700

    Rename function from articleView to contentView

--- javascript/constitution.js
+++ javascript/constitution.js
@@ -1,5 +1,5 @@
 // This function hides or reveals the content of each article
-function articleView(view) {
+function contentView(view) {
 	if (view.style.display === "block") {
 		view.style.display = "none";
 	} else {
@@ -18,34 +18,34 @@ const onClick = function() {
 	// Find which button was clicked
 	if (button === "btn-preamble") {
 		status = document.getElementById("preamble-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article1") {
 		status = document.getElementById("article1-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article2") {
 		status = document.getElementById("article2-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article3") {
 		status = document.getElementById("article3-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article4") {
 		status = document.getElementById("article4-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article5") {
 		status = document.getElementById("article5-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article6") {
 		status = document.getElementById("article6-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article7") {
 		status = document.getElementById("article7-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article8") {
 		status = document.getElementById("article8-content");
-		articleView(status);
+		contentView(status);
 	} else if (button === "btn-article9") {
 		status = document.getElementById("article9-content");
-		articleView(status);
+		contentView(status);
 	}
 }
 
